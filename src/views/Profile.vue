504e0f6fdc2aeb254112d9f820ef91222ac7b744
sato3028 <sakayusho007@gmail.com>
2023-09-13 08:16:43 +0000

    profile-add

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,45 +2,11 @@
   <div>
     <div class="ui main container">
       <div class="ui segment">
-        <!-- 基本的なコンテンツはここに記載する -->
-        <div class="submitted" v-if="done_submit">
-          <h2>プロフィール情報を更新しました</h2>
-        </div>
-        <form class="ui large form" @submit.prevent="submit">
-          <div class="field">
-            <div class="ui left icon input">
-              <i class="user icon"></i>
-              <input v-model="user.userId" type="text" placeholder="ログインID" required disabled>
-            </div>
-          </div>
-          <div class="field">
-            <div class="ui left icon input">
-              <i class="lock icon"></i>
-              <input v-model="user.password" type="password" placeholder="パスワード">
-            </div>
-          </div>
-          <div class="field">
-            <div class="ui left icon input">
-              <i class="tag icon"></i>
-              <input v-model="user.name" type="name" placeholder="赤ちゃんの名前">
-            </div>
-          </div>
-          <div class="field">
-            <div class="ui left icon input">
-              <i class="calendar icon"></i>
-              <input v-model.number="user.age" type="text" placeholder="age">
-            </div>
-          </div>
-          <button :disabled="isButtonDisabled" @click="submit()" class="ui fluid green huge button" type="submit">
-            更新
-          </button>
+        
+        <form>
         </form>
+        
       </div>
-      <!--
-      <button @click="deleteUser" class="ui huge grey fluid button" type="submit">
-        退会
-      </button>
-      -->
     </div>
   </div>
 </template>
@@ -155,37 +121,38 @@ export default {
       }
     },
     
-    async submit() {
+async submit() {
       const headers = {'Authorization': 'mtiToken'};
-      
-      const { userId, password, nickname, age } = this.user;
+
+      const { userId, password, name, dislike, season } = this.user;
       const reqBody = {
         userId,
         password,
-        nickname,
-        age
+        name,
+        dislike,
+        season
       };
-      
+
       try {
         const res = await fetch(baseUrl + '/user', {
           method: 'PUT',
           body: JSON.stringify(reqBody),
           headers,
         });
-        
+
         const text = await res.text();
         const jsonData = text ? JSON.parse(text) : {}
-        
+
         if (!res.ok) {
           const errorMessage = jsonData.message ?? 'エラーメッセージがありません';
           throw new Error(errorMessage);
         }
-        
+
         if (res.status == 200){
           this.done_submit = true;
         }
         console.log(jsonData);
-        
+
       } catch (e) {
         console.log(e)
       }
